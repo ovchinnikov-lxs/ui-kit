656c07d3c9e661ea4b61b0bb5d016238e0d3c081
ovchinnikov-lxs <ovchinnikov.lxs@gmail.com>
2023-02-16 19:11:28 +0300

    fix(TypeSize): Убрал строгие размеры

--- src/assets/utils/types/index.ts
+++ src/assets/utils/types/index.ts
@@ -1,7 +1,7 @@
 import type { IOptionItem } from '~/assets/utils/interfaces';
 
 type TypeClassList = Array<string | object>;
-type TypeSize = 'x-small' | 'small' | 'medium' | 'large' | 'x-large' | 'xx-large' | 'custom';
+type TypeSize = 'x-small' | 'small' | 'medium' | 'large' | 'x-large' | 'xx-large' | 'custom' | string;
 type TypeOptions = Array<IOptionItem>
 
 export type {
