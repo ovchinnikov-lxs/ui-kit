0ccbfce2df83ddacc6ddfa2350d0d8ba933f19f6
ovchinnikov-lxs <ovchinnikov.lxs@gmail.com>
2023-02-23 00:28:20 +0300

    fix(lint): Поправил ошибку линтера

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,6 @@
 declare module '*.vue' {
     import type { DefineComponent } from 'vue';
-    const component: DefineComponent<{}, {}, any>;
+    const component: ReturnType<typeof DefineComponent>;
+
     export default component;
 }
