0de4c84ce9adcdbbe009e27fbb0126fd0c3f291c
ovchinnikov-lxs <ovchinnikov.lxs@gmail.com>
2023-02-23 00:25:32 +0300

    fix(unit-tests): Поправил тест

--- __tests__/composables/useClassName.spec.ts
+++ __tests__/composables/useClassName.spec.ts
@@ -5,6 +5,6 @@ describe('Проверка useClassName', () => {
         const CLASS_NAME = 'Test';
 
         const res = useClassName({ classPrefix: CLASS_NAME });
-        expect(res.getClassName.value(CLASS_NAME)).toEqual([{ OTest: 'O', TestTest: true }]);
+        expect(res.getClassName.value(CLASS_NAME)).toEqual([{ UiTest: 'Ui', TestTest: true }]);
     });
 });
