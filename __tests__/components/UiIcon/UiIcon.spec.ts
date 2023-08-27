785399b2fb7d16bdc433d3435bd946fff08097f1
ovchinnikov-lxs <ovchinnikov.lxs@gmail.com>
2023-08-27 12:42:02 +0400

    chore(test): Changed the testing infrastructure

--- __tests__/components/UiIcon/UiIcon.spec.ts
+++ __tests__/components/UiIcon/UiIcon.spec.ts
@@ -1,15 +1,14 @@
-// import { render } from '@testing-library/vue';
-// import UiIcon from '../../../src/components/UiIcon/UiIcon.vue';
-//
-// describe('Test UiIcon component', () => {
-//     it('it should work', () => {
-//         const res = render(UiIcon, {
-//             props: {
-//                 /* ... */
-//             },
-//         });
-//     });
-//     it('should be some thing', () => {
-//         console.log('lol');
-//     });
-// });
+import { render } from '@testing-library/vue';
+import UiIcon from '../../../src/components/UiIcon/UiIcon.vue';
+
+describe('Test UiIcon component', () => {
+    it('it should work', () => {
+        const { debug } = render(UiIcon, {
+            props: {
+                name: 'close',
+            },
+        });
+
+        debug();
+    });
+});
